7ec07e52e8e738f4d506c7601a376fb18b3bb677
Mikhail Zabaluev <mikhail.zabaluev@gmail.com>
2013-04-09 08:52:39 +0300

    Added a rustpkg build file, which does not yet work
    Building multiple crates with dependencies between them is apparently
    not supported yet.

---
+++ pkg.rs
@@ -0,0 +1,6 @@
+#[pkg(id="io.github.mzabaluev.grust", vers="0.1.0")];
+
+#[pkg_crate(file = "grust/grust.rc")];
+#[pkg_crate(file = "glib.rc")];
+#[pkg_crate(file = "gobject.rc")];
+#[pkg_crate(file = "gio.rc")];
